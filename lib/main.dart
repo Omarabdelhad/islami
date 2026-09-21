2ef673584d1537d13f2dbca9584e1906b3c25758
Omarabdelhad <asd65563@gmail.com>
2026-09-21 16:40:29 +0300

    add assets on_boarding ,test push

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:islami_c20/ui/hadeth_details/screen/hadeth_details_screen.dart';
-import 'package:islami_c20/ui/home/screen/home_screen.dart';
-import 'package:islami_c20/ui/sura_details/screen/sura_details_screen.dart';
-
-import 'core/resources/routes_manager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,12 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner:false,
-      routes: {
-        RoutesManager.homeRouteName:(context)=>HomeScreen(),
-        RoutesManager.suraDetailsRouteName:(context)=>SuraDetailsScreen(),
-        RoutesManager.hadethDetailRouteName:(context)=>HadethDetailsScreen(),
-      },
-      initialRoute: RoutesManager.homeRouteName,
+      routes: {},
     );
   }
 }
