109a3090ba90c9f4cc6d9264ff6b103d8bf97663
Omarabdelhad <asd65563@gmail.com>
2026-09-23 00:42:54 +0300

    finsh sebha tab

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -8,7 +8,8 @@ abstract final class AssetsManager {
   static const String header = "assets/images/img_header.png";
   static const String hadethRightCorner = "assets/images/right_corner.png";
   static const String hadethLeftCorner = "assets/images/left_corner.png";
-  static const String hadethCardBack = "assets/images/HadithCardBackGround 1.png";
+  static const String hadethCardBack =
+      "assets/images/HadithCardBackGround 1.png";
   static const String hadethMosque = "assets/images/hadeth_mosque.png";
   static const String leftCorner = "assets/images/img_left_corner.png";
   static const String rightCorner = "assets/images/img_right_corner.png";
@@ -16,5 +17,7 @@ abstract final class AssetsManager {
   static const String quranBack = "assets/images/quran_back.png";
   static const String mostRecent = "assets/images/img_most_recent.png";
   static const String suraNumber = "assets/images/sura_number.svg";
-  static const String sebhaBack = "assets/images/sebha_bg.png";
+  static const String sebhaBack = "assets/images/sebhaBack.png";
+  static const String sebhabody = "assets/images/SebhaBody.png";
+  static const String sebhaheader = "assets/images/sebhaHeader.png";
 }
--- lib/ui/home/tabs/sebha/sebha_tab.dart
+++ lib/ui/home/tabs/sebha/sebha_tab.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:islami_c20/core/resources/assets_manager.dart';
 import 'package:islami_c20/core/resources/colors_manager.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
+
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+  int zkrIndex = 0;
+  double turns = 0;
+
+  final List<String> azkar = [
+    'سبحان الله',
+    'الحمد لله',
+    'لا إله إلا الله',
+    ' الله أكبر',
+  ];
+
+  void incrementconter() {
+    setState(() {
+      counter++;
+      turns += 1 / 33;
+      if (counter == 34) {
+        counter = 0;
+        zkrIndex = ((zkrIndex + 1) % azkar.length);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +38,7 @@ class SebhaTab extends StatelessWidget {
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage(AssetsManager.sebhaBack),
-          colorFilter: ColorFilter.mode(
-            Color(0xff202020B2).withValues(alpha: .70),
-            BlendMode.darken,
-          ),
+
           fit: BoxFit.cover,
         ),
       ),
@@ -32,8 +56,64 @@ class SebhaTab extends StatelessWidget {
                 color: ColorsManager.whiteColor,
               ),
             ),
-                        SizedBox(height: 16),
+            SizedBox(height: 16),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    bottom: 359,
+                    right: 0,
+                    left: 0,
+                    child: Image.asset(AssetsManager.sebhaheader),
+                  ),
 
+                  Positioned(
+                    top: 70,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+
+                    child: GestureDetector(
+                      onTap: incrementconter,
+                      child: AnimatedRotation(
+                        turns: turns,
+                        duration: Duration(milliseconds: 300),
+                        child: Image.asset(AssetsManager.sebhabody),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const SizedBox(height: 90),
+                          Text(
+                            azkar[zkrIndex],
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: .w700,
+                              color: ColorsManager.whiteColor,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+
+                          Text(
+                            '$counter',
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: .w700,
+                              color: ColorsManager.whiteColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
